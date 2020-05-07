b3f5cae2950365c9fe118331d1b9ce8d7d58389b
Marcus Koh <kohchihao@gmail.com>
2020-05-07 20:34:20 +0800

    deploy-dokku.sh: Update path error

--- config/travis/deploy-dokku.sh
+++ config/travis/deploy-dokku.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 openssl aes-256-cbc -K $encrypted_9d581fdc29c5_key -iv $encrypted_9d581fdc29c5_iv -in .travis/dokku-deploy-admin.key.enc -out .travis/dokku-deploy-admin.key -d
 eval "$(ssh-agent -s)";
-chmod 600 .travis/dokku-deploy.key;
-ssh-add .travis/dokku-deploy.key;
+chmod 600 .travis/dokku-deploy-admin.key.key;
+ssh-add .travis/dokku-deploy-admin.key.key;
 ssh-keyscan "$DOKKU_HOST" >> ~/.ssh/known_hosts;
 git remote add deploy dokku@"$DOKKU_HOST":"$DOKKU_APP";
 git config --global push.default simple;
