8a79b89ceb4ae7ffb0a4d1b8b924d88585db40bd
Marcus Koh <kohchihao@gmail.com>
2020-07-04 16:49:20 +0800

    Add firebase and constants

---
+++ utils/constants/siteUrl.js
@@ -0,0 +1,12 @@
+let BASE_URL = '';
+let FIREBASE_EMAIL_ACTION_URL = '';
+
+if (process.env.NODE_ENV === 'development') {
+  BASE_URL = 'http://localhost:3000';
+  FIREBASE_EMAIL_ACTION_URL = 'http://localhost:3000';
+} else {
+  BASE_URL = 'https://admin-gfg.herokuapp.com/';
+  FIREBASE_EMAIL_ACTION_URL = 'https://admin-gfg.herokuapp.com/';
+}
+
+export { BASE_URL, FIREBASE_EMAIL_ACTION_URL };
