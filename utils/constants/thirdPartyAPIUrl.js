9c2b925903115b91a4c202fec2b4ecc5753248f1
Marcus Koh <kohchihao@gmail.com>
2020-09-09 21:24:44 +0800

    Update to nextjs9.5

--- utils/constants/thirdPartyAPIUrl.js
+++ utils/constants/thirdPartyAPIUrl.js
@@ -1,1 +1,1 @@
-export const GEO_LOCATION_URL = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.FIREBASE_API_KEY}`;
+export const GEO_LOCATION_URL = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`;
