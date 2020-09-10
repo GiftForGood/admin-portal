e8f76100bb34947d4269c41e5355035b2d0adcf2
Jin Ying <kenneth.jyt@gmail.com>
2020-09-10 13:15:53 +0800

    AdminsAPI: Add user roles

--- utils/constants/admin.js
+++ utils/constants/admin.js
@@ -8,3 +8,12 @@ export const ORDER_BY = {
 export const isValidOrderBy = (orderBy) => {
   return Object.values(ORDER_BY).includes(orderBy);
 };
+
+export const ADMIN_ROLE = 'admin';
+export const ADMIN_EDITOR_ROLE = 'adminEditor';
+export const ADMIN_VERIFIER_ROLE = 'adminVerifier';
+export const ROLES = [ADMIN_ROLE, ADMIN_EDITOR_ROLE, ADMIN_VERIFIER_ROLE];
+
+export const isValidRole = (role) => {
+  return ROLES.includes(role);
+};
