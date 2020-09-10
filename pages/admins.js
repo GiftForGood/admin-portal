4da3be2a190a4659d91e742e82e9d033ede654c9
Marcus Koh <kohchihao@gmail.com>
2020-09-10 16:50:47 +0800

    Allow admin only on /admins

--- pages/admins.js
+++ pages/admins.js
@@ -3,12 +3,14 @@ import { isAuthenticated } from '@utils/authentication/authentication';
 import SessionProvider from '@components/session/modules/SessionProvider';
 import MaxWidthContainer from '@components/containers/MaxWidthContainer';
 import AdminsPages from '@components/admins/pages/AdminsPage';
+import { isAdmin } from '@utils/authentication/checkAdminType';
 
 import dynamic from 'next/dynamic';
 const TopNavigationBar = dynamic(() => import('@components/navbar/modules/TopNavigationBar'), { ssr: false });
 
 export async function getServerSideProps({ params, req, res, query }) {
   let data = await isAuthenticated(req, res, { Location: '/' });
+  isAdmin(data.user, res, { Location: '/dashboard' });
   return {
     props: {
       user: data.user || null,
