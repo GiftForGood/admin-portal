73876d8a9c37f9657bd8aebe26391500505668e1
Tan Jin Ying <kenneth.jyt@gmail.com>
2020-09-11 22:33:34 +0800

    NPOVerificationsAPI: Fix current admin error (#37)

--- api/common/currentUser.js
+++ api/common/currentUser.js
@@ -13,7 +13,51 @@ export const getCurrentAdmin = async () => {
 
   const token = await user.getIdTokenResult();
   if (!token.claims.admin) {
-    throw new CurrentUserError('invalid-current-user', 'current user is not an admin');
+    throw new CurrentUserError('invalid-current-user', 'current user does not have admin role');
+  }
+
+  const snapshot = await db.collection('administrators').doc(user.uid).get();
+  if (!snapshot.exists) {
+    throw new CurrentUserError('invalid-current-user', 'admin does not exist');
+  }
+  return snapshot.data();
+};
+
+export const getCurrentAdminEditorAndAbove = async () => {
+  const user = firebaseAuth.currentUser;
+
+  if (user === null) {
+    throw new CurrentUserError('invalid-current-user', 'invalid current user');
+  }
+  if (!user.emailVerified) {
+    throw new CurrentUserError('invalid-current-user', 'current user email not verified');
+  }
+
+  const token = await user.getIdTokenResult();
+  if (!token.claims.admin && !token.claims.adminEditor) {
+    throw new CurrentUserError('invalid-current-user', 'current user does have editor role and above');
+  }
+
+  const snapshot = await db.collection('administrators').doc(user.uid).get();
+  if (!snapshot.exists) {
+    throw new CurrentUserError('invalid-current-user', 'admin does not exist');
+  }
+  return snapshot.data();
+};
+
+export const getCurrentAdminVerifierAndAbove = async () => {
+  const user = firebaseAuth.currentUser;
+
+  if (user === null) {
+    throw new CurrentUserError('invalid-current-user', 'invalid current user');
+  }
+  if (!user.emailVerified) {
+    throw new CurrentUserError('invalid-current-user', 'current user email not verified');
+  }
+
+  const token = await user.getIdTokenResult();
+  if (!token.claims.admin && !token.claims.adminEditor && !token.claims.adminVerifier) {
+    throw new CurrentUserError('invalid-current-user', 'current user does not have verifier role and above');
   }
 
   const snapshot = await db.collection('administrators').doc(user.uid).get();
