5570b89ded2863ad8f972bfbba87e3a42955efbc
Marcus Koh <kohchihao@gmail.com>
2020-09-12 18:32:27 +0800

    Add legal API

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ import AuthAPI from './auth';
 import AdminsAPI from './admins';
 import NPOVerifications from './npoVerifications';
 import NPOOrganizations from './npoOrganizations';
+import LegalAPI from './legal'; 
 
 class API {
   auth = new AuthAPI();
   admins = new AdminsAPI();
   npoVerifications = new NPOVerifications();
   npoOrganizations = new NPOOrganizations();
+  legal = new LegalAPI();
 }
 
 const api = new API();
