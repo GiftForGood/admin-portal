e0284f141f7b47a78f10eeae198c4bdc77dcdcc6
Marcus Koh <kohchihao@gmail.com>
2020-09-12 18:41:51 +0800

    Add editor for legal docs

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { createWrapper } from 'next-redux-wrapper';
 import store from '../store';
 import 'react-toastify/dist/ReactToastify.css';
+import "react-quill/dist/quill.snow.css";
 
 const GlobalStyle = createGlobalStyle`
   body {
