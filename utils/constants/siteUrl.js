5ad6bb1189dfb1a2e2ec2503875aab4b468f7ed6
Jin Ying <kenneth.jyt@gmail.com>
2020-09-27 23:52:22 +0800

    Update cloud functions url for localhost

--- utils/constants/siteUrl.js
+++ utils/constants/siteUrl.js
@@ -1,13 +1,15 @@
 let BASE_URL = '';
 let FIREBASE_EMAIL_ACTION_URL = '';
-const CLOUD_FUNCTIONS_URL = 'https://us-central1-giftforgood.cloudfunctions.net/';
+let CLOUD_FUNCTIONS_URL;
 
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://localhost:3001';
   FIREBASE_EMAIL_ACTION_URL = 'http://localhost:3001';
+  CLOUD_FUNCTIONS_URL = 'http://localhost:5001/giftforgood/us-central1'
 } else {
   BASE_URL = 'https://admin-gfg.herokuapp.com/';
   FIREBASE_EMAIL_ACTION_URL = 'https://admin-gfg.herokuapp.com/';
+  CLOUD_FUNCTIONS_URL = 'https://us-central1-giftforgood.cloudfunctions.net/'
 }
 
 export { BASE_URL, FIREBASE_EMAIL_ACTION_URL, CLOUD_FUNCTIONS_URL };
